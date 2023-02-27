7c07e9f1356c9cdca7719c8ef9420c5ad40462f5
wojnoski <karol.wojnarowski@gmail.com>
2023-02-27 13:16:33 +0100

    Init accordion

--- src/js/script.js
+++ src/js/script.js
@@ -57,6 +57,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('New Product: ', thisProduct);
     }
     renderInMenu(){
@@ -70,6 +71,26 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+    /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('Trigger: ', clickableTrigger);
+    /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+        event.preventDefault();
+      /* find active product (product that has active class) */
+        const ActiveProduct = document.querySelector(select.all.menuProductsActive);
+        console.log(ActiveProduct);
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (ActiveProduct != null && ActiveProduct != thisProduct.element){
+          ActiveProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+      /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
   const app = {
