28dfe2b70c56c6223e43f9425e9affb93ab0de90
wojnoski <karol.wojnarowski@gmail.com>
2023-03-20 13:10:04 +0100

    Subpages order and booking

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,47 @@ import Product from './components/product.js';
 import Cart from './components/Cart.js';
 
 export const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const IDFromHash = window.location.hash.replace('#/', '');
+    console.log('id: ', IDFromHash);
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages){
+      if (page.id == IDFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /* get page if from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  activatePage: function(pageID){
+    const thisApp = this;
+    /* add class active to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageID);
+    }
+    /* add class active to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageID
+      );
+    }
+  },
   initMenu: function(){
     const thisApp = this;
     console.log('thisAppdata: ', thisApp.data);
@@ -45,6 +86,7 @@ export const app = {
     console.log('templates:', templates);
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initPages();
   },
 };
 app.init();
