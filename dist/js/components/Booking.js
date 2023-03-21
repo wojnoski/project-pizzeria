d8443359447149e7510cbf827acb3bee4436e207
wojnoski <karol.wojnarowski@gmail.com>
2023-03-21 20:48:07 +0100

    11.1 finished - dates and hours

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {select, templates} from '../settings.js';
 import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element){
@@ -17,6 +19,8 @@ class Booking {
     thisBooking.dom.wrapper.appendChild(thisBooking.element);
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
   }
   initWidgets(){
     const thisBooking = this;
@@ -24,6 +28,8 @@ class Booking {
     thisBooking.dom.peopleAmount.addEventListener('updated', function(){});
     thisBooking.AmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.dom.hoursAmount.addEventListener('updated', function(){});
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
 
